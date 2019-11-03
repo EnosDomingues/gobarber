f0478b347addfc474d00e6ffc6dd0f17715f7328
Enos Domingues <enosdomingues@gmail.com>
2019-11-03 10:41:49 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8f23394a7d8dea',
+    pass: '694843e53b2a33',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
