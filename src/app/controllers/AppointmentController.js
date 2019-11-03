a2009328697f11d9aa30f0f19b6de7f7339f13a1
Enos Domingues <enosdomingues@gmail.com>
2019-11-03 11:53:04 -0300

    Configurando templates de e-mail - incompleto

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -121,6 +121,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -145,7 +150,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.emai}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      contex: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
